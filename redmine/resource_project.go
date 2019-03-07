266c97319e6a5b835d26ce3c8c20a0715af5a115
KrlWil <willems.karel@hotmail.com>
2019-03-07 17:18:45 +0100

    add created_on, updated_on

--- redmine/resource_project.go
+++ redmine/resource_project.go
@@ -1,57 +1,74 @@
 package redmine
 
 import (
-	redmine "github.com/mattn/go-redmine"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/pkg/errors"
+    redmine "github.com/mattn/go-redmine"
+    "github.com/hashicorp/terraform/helper/schema"
+    "github.com/pkg/errors"
 )
 
 // resourceProject: used to map a Redmine project to a terraform schema
 func resourceProject() *schema.Resource {
-	return &schema.Resource{
-		Create: resourceProjectCreate,
-		Read:   resourceProjectRead,
-		Update: resourceProjectUpdate,
-		Delete: resourceProjectDelete,
+    return &schema.Resource{
+        Create: resourceProjectCreate,
+        Read:   resourceProjectRead,
+        Update: resourceProjectUpdate,
+        Delete: resourceProjectDelete,
 
-		Schema: map[string]*schema.Schema{
-			"name": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"description": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"identifier": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-			},
-		},
-	}
+        Schema: map[string]*schema.Schema{
+            "name": &schema.Schema{
+                Type:     schema.TypeString,
+                Required: true,
+            },
+            "identifier": &schema.Schema{
+                Type:     schema.TypeString,
+                Required: true,
+            },
+            "description": &schema.Schema{
+                Type:     schema.TypeString,
+                Optional: true,
+                Default:  "",
+            },
+            "created_on": &schema.Schema{
+                Type:     schema.TypeString,
+                Optional: true,
+                Default:  "",
+            },
+            "updated_on": &schema.Schema{
+                Type:     schema.TypeString,
+                Optional: true,
+                Default:  "",
+            },
+        },
+    }
 }
 
 // resourceProjectCreate creates a new redmine project using the redmine REST api
 func resourceProjectCreate(d *schema.ResourceData, m interface{}) error {
         config := m.(*Config)
         name := d.Get("name").(string)
-        description := d.Get("description").(string)
         identifier := d.Get("identifier").(string)
+        description := d.Get("description").(string)
+        createdOn := d.Get("created_on").(string)
+        updatedOn := d.Get("updated_on").(string)
 
         i := redmine.Project{
                         Name: name,
-                        Description: description,
                         Identifier: identifier,
+                        Description: description,
+                        CreatedOn: createdOn,
+                        UpdatedOn: updatedOn,
         }
         project, err := config.redmineClient.CreateProject(i)
         if err != nil {
                 return errors.Wrap(err, "creating redmine project failed")
         }
-        if project != nil {
-                return nil
-        }
 
-        return nil
+        //convert project.Id to string, setId() takes string
+        s1 := strconv.Itoa(project.Id)
+
+        d.SetId(s1)
+
+        return resourceProjectRead(d, m)
 }
 
 func resourceProjectRead(d *schema.ResourceData, m interface{}) error {
